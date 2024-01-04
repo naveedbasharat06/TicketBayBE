f98199945518b44b98420ad46e00edb5dcdf0939
naveedbasharat06 <naveed.basharat06@gmaill.com>
2024-01-04 23:11:43 +0500

    updated database config to success deployment

--- config/database.js
+++ config/database.js
@@ -51,7 +51,7 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: os.hostname() || env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
